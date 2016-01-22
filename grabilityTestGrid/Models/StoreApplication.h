8eb02e5aae577e92415a6cec4b05f36e0e24dc57
John Edwin Guerrero Ayala <ingjohnguerrero@gmail.com>
2016-01-21 20:15:27 -0500

    Adición de modelo de aplicación

---
+++ grabilityTestGrid/Models/StoreApplication.h
@@ -0,0 +1,13 @@
+//
+//  StoreApplication.h
+//  grabilityTestGrid
+//
+//  Created by John Edwin Guerrero Ayala on 1/21/16.
+//  Copyright © 2016 ingjohnguerrero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StoreApplication : NSObject
+
+@end
---
+++ grabilityTestGrid/Models/StoreApplication.m
@@ -0,0 +1,13 @@
+//
+//  StoreApplication.m
+//  grabilityTestGrid
+//
+//  Created by John Edwin Guerrero Ayala on 1/21/16.
+//  Copyright © 2016 ingjohnguerrero. All rights reserved.
+//
+
+#import "StoreApplication.h"
+
+@implementation StoreApplication
+
+@end
